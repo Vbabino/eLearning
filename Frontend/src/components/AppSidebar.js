e5a2b3ccfcbfd21eaedb1accd21619a9e0e4375a
Vittorio Babino <vittoriobabino@gmail.com>
2025-02-22 17:45:18 +0000

    Update title and favicon for E-Learning App; modify sidebar branding

--- Frontend/src/components/AppSidebar.js
+++ Frontend/src/components/AppSidebar.js
@@ -8,13 +8,16 @@ import {
   CSidebarFooter,
   CSidebarHeader,
   CSidebarToggler,
+  CContainer,
+  CRow,
+  CCol,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
+import {
+  cilEducation,
+} from '@coreui/icons'
 
 import { AppSidebarNav } from './AppSidebarNav'
-
-import { logo } from 'src/assets/brand/logo'
-import { sygnet } from 'src/assets/brand/sygnet'
 
 // sidebar nav config
 import navigation from '../_nav'
@@ -37,8 +40,16 @@ const AppSidebar = () => {
     >
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+          <CContainer>
+            <CRow>
+              <CCol>
+                <CIcon icon={cilEducation} height={32} />
+              </CCol>
+              <CCol>
+                <p>E-Learning App</p>
+              </CCol>
+            </CRow>
+          </CContainer>
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
